d6b84abccc3e210fc7a000bea3039c0601f3d8f2
Eelco Wiersma <eelco@pagebakers.nl>
2015-12-04 10:46:56 +0100

    Export on server only

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: "FullContact wrapped for Meteor.",
   name: 'forwarder:fullcontact',
-  version: '1.0.0'
+  version: '1.0.0_1'
 });
 
 Npm.depends({
@@ -10,7 +10,7 @@ Npm.depends({
 
 Package.onUse(function (api) {
   api.versionsFrom('1.0');
-  
+
   api.use([
     'underscore'
   ], 'server');
@@ -20,6 +20,6 @@ Package.onUse(function (api) {
   ], 'server');
 
   if(api.export) {
-    api.export('FullContact');
+    api.export('FullContact', 'server');
   }
 });
